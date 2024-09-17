9f6ce04ffde85ed896925c89c02049df3a152e64
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-09-17 16:53:33 -0300

    update js and html

--- JS/login.js
+++ JS/login.js
@@ -1,23 +1,21 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
   const login = document.querySelector(".btn-entrar");
-  
+
   login.addEventListener("click", () => {
     window.location.href = "home.html";
   });
 
-  const themeSelect = document.querySelector(".themes")
+  const themeSelect = document.querySelector(".themes");
 
-  themeSelect.addEventListener("change", (e)=>{
-    const theme = e.target.value;
+  themeSelect.addEventListener("change", (e) => {
+    const selected = e.target.value;
 
-    if(theme === "light"){
-      document.body.style.backgroundColor = "#FFF"
-    }else{
+    if(selected === "dark"){
       document.body.style.backgroundColor = "#0F1216"
+    }else if(selected === "light"){
+      document.body.style.backgroundColor = "#F7F7F7"
+    }else{
+      return selected;
     }
-  })
-
-})
-
-
-
+  });
+});
